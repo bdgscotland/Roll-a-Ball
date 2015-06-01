dae83ca06d3cf81db658b7324c71b3a70ad109ab
bdgscotland <duncan@bowring.us>
2015-05-31 17:49:28 -0700

    First commit
    Roll-a-ball mini game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () // fixed update - just before any physics ops - physics code here - rigidbody code here
+	{
+		// http://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+
+	}
+
+
+
+
+
+}
